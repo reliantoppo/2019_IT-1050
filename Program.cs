a16a49fe800b6f93219b655407f0680b30b73a81
Neil M <workwithnmadaczky@gmail.com>
2019-10-24 10:48:48 -0400

    week1_assignment
     This program multiplies two numbers. shows working with strings and doing math on variables.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week1NeilMadaczky
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // This program multiplies two numbers.
+
+            string str1, str2;
+            str1 = "Hello world";
+            str2 = "Neil Madaczky";
+            
+            int num1, num2, num3;
+
+            num1 = 27;
+            num2 = 55;
+
+            num3 = num1 * num2;
+
+            Console.WriteLine("The product of " + "num1" + " and " + num2 + " is " + num3);
+            Console.WriteLine(str1 + " from " + str2);
+		}
+	}
+}
